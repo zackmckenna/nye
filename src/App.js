486ac7209c288cb5cee2d5c43c5871ee36e4bfeb
Zack McKenna <zackmckenna@gmail.com>
2020-12-27 14:08:29 -0500

    test one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,215 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data";
+import { useEffect, useState } from "react";
+import {
+  Accordion,
+  Jumbotron,
+  Container,
+  Col,
+  Row,
+  Card,
+  Button,
+} from "react-bootstrap";
 
-function App() {
+const App = () => {
+  const [characters, setCharacters] = useState();
+  const processData = (data) => {
+    const cleanedData = [];
+    data.map((answers) => {
+      const user = {};
+      for (const [key, value] of Object.entries(answers)) {
+        switch (key) {
+          case "Before we start, what is your real life name?":
+            user.username = value;
+            break;
+          case "What is your character's name?":
+            user.characterName = value;
+            break;
+          case "What is your race?":
+            user.race = value;
+            break;
+          case "What is your role?":
+            user.role = value.toLowerCase();
+          case "What is your descriptor?":
+            user.descriptor = value.toLowerCase();
+            break;
+          case "What is your strength?":
+            user.strength = value.toLowerCase();
+            break;
+          case "What is your flaw?":
+            user.flaw = value.toLowerCase();
+            break;
+          case "What is your drive?":
+            user.drive = value.toLowerCase();
+            break;
+          case "Throughout your life, you have gained a title, nickname or honorific. What is it, and how did you gain it?":
+            user.nickname = value;
+            break;
+          case "While everyone is excited to be invited to the great Yule Festival, there is another motivation that drives you to attend. What is it?":
+            user.motivation = value;
+            break;
+          case "Of all the places you’ve seen on your travels, one stands out and you long to return. Where is it and why?":
+            user.memory = value;
+          case "How are you known within the Realms? Are you famous, infamous, a normal citizen or a mystery?":
+            user.infamy = value;
+            break;
+          case "What is your home Realm like? What is something you love about it? What about it do you wish would change?":
+            user.home = value;
+        }
+      }
+      cleanedData.push(user);
+    });
+    return cleanedData;
+  };
+
+  const style = {
+    pink: {
+      color: "magenta",
+    },
+    green: {
+      color: "#5cb85c",
+    },
+    red: {
+      color: "#d9534f",
+    },
+    blue: {
+      color: "#5bc0de",
+    },
+    orange: {
+      color: "#f0ad4e",
+    },
+  };
+
+  useEffect(() => {
+    setCharacters(processData(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Jumbotron fluid>
+        <Container>
+          <h1>The Great Yule Ball</h1>
+          <p>The attendees of the great Yule Ball of 1202</p>
+        </Container>
+      </Jumbotron>
+      <Container>
+        {characters &&
+          characters.map((character) => {
+            return (
+              <Row className="mb-3">
+                <Col>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title style={{ fontFamily: "Langar" }}>
+                        {character.characterName
+                          ? character.characterName
+                          : "name unknown"}
+                      </Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">
+                        {character.race ? character.race : ""}
+                      </Card.Subtitle>
+
+                      <Card.Text>
+                        <p>
+                          A {character.descriptor} {character.role} who has{" "}
+                          <span style={style.green}>{character.strength}</span>,
+                          but also{" "}
+                          <span style={style.red}>{character.flaw}</span>. Thier
+                          drive is to {character.drive}.
+                        </p>
+                      </Card.Text>
+                      <Accordion>
+                        {character.nickname && (
+                          <Card>
+                            <Card.Header>
+                              <Accordion.Toggle
+                                as={Button}
+                                variant="link"
+                                eventKey="0"
+                              >
+                                How did you gain your title?
+                              </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="0">
+                              <Card.Body>{character.nickname}</Card.Body>
+                            </Accordion.Collapse>
+                          </Card>
+                        )}
+                        {character.motivation && (
+                          <Card>
+                            <Card.Header>
+                              <Accordion.Toggle
+                                as={Button}
+                                variant="link"
+                                eventKey="1"
+                              >
+                                What is your motivation for attending?
+                              </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="1">
+                              <Card.Body>{character.motivation}</Card.Body>
+                            </Accordion.Collapse>
+                          </Card>
+                        )}
+                        {character.memory && (
+                          <Card>
+                            <Card.Header>
+                              <Accordion.Toggle
+                                as={Button}
+                                variant="link"
+                                eventKey="2"
+                              >
+                                What is a memorable location from your travels?
+                              </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="2">
+                              <Card.Body>{character.memory}</Card.Body>
+                            </Accordion.Collapse>
+                          </Card>
+                        )}
+                        {character.infamy && (
+                          <Card>
+                            <Card.Header>
+                              <Accordion.Toggle
+                                as={Button}
+                                variant="link"
+                                eventKey="3"
+                              >
+                                What is your status in the Realm?
+                              </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="3">
+                              <Card.Body>{character.infamy}</Card.Body>
+                            </Accordion.Collapse>
+                          </Card>
+                        )}
+                        {character.home && (
+                          <Card>
+                            <Card.Header>
+                              <Accordion.Toggle
+                                as={Button}
+                                variant="link"
+                                eventKey="4"
+                              >
+                                What is your home like?
+                              </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="4">
+                              <Card.Body>{character.home}</Card.Body>
+                            </Accordion.Collapse>
+                          </Card>
+                        )}
+                      </Accordion>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            );
+          })}
+      </Container>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,92 @@
+const data = [
+  {
+    Timestamp: "2020/12/20 8:23:56 PM EST",
+    Username: "",
+    "Before we start, what is your real life name?": "Ali",
+    "What is your character's name?": "Kifnali Rosebeam",
+    "What is your race?": "Human",
+    "What is your role?": "Mystic/Sorceror",
+    "What is your descriptor?": "Gentle",
+    "What is your strength?": "An affinity with animals & beasts",
+    "What is your flaw?": "Enemies, too numerous to count",
+    "What is your drive?": "Bring peace to this chaotic land",
+    "Throughout your life, you have gained a title, nickname or honorific. What is it, and how did you gain it?":
+      "",
+    "While everyone is excited to be invited to the great Yule Festival, there is another motivation that drives you to attend. What is it?":
+      "I must ensure balance in order to bring peace to the realms. I had a vision that my attendance would prevent devastation lasting 100 years. I must make sure 2 other people in attendance never meet each other, but I don’t know who they are.",
+    "Of all the places you’ve seen on your travels, one stands out and you long to return. Where is it and why?":
+      "",
+    "How are you known within the Realms? Are you famous, infamous, a normal citizen or a mystery?":
+      "Famous and infamous. Generally the public respects me, but I continuously upset a lot of people in my life’s quest to maintain peace.",
+    "What is your home Realm like? What is something you love about it? What about it do you wish would change?":
+      "",
+  },
+  {
+    Timestamp: "2020/12/20 8:35:02 PM EST",
+    Username: "",
+    "Before we start, what is your real life name?": "Chris",
+    "What is your character's name?": "Libold",
+    "What is your race?": "Human",
+    "What is your role?": "Swordsman",
+    "What is your descriptor?": "Hardened",
+    "What is your strength?": "Astonishingly fast reflexes",
+    "What is your flaw?": "An obligation to thier family",
+    "What is your drive?": "Repent for past wrongdoings",
+    "Throughout your life, you have gained a title, nickname or honorific. What is it, and how did you gain it?":
+      "Libold the loyal",
+    "While everyone is excited to be invited to the great Yule Festival, there is another motivation that drives you to attend. What is it?":
+      "While Kifnali’s motives for attending are different (yet related 😉), I’ve heard rumor that a person I thought long dead is actually very much alive, and may be at this event. This concerns me because they have knowledge of the misdeeds from my past I’ve worked so hard to overcome. ",
+    "Of all the places you’ve seen on your travels, one stands out and you long to return. Where is it and why?":
+      "The Port city of Essona, where I committed a horrific act that still haunts me. I am determined to make peace with myself, but I am unsure of any loose ends that could potentially unravel all the work I’ve done to forget my past. ",
+    "How are you known within the Realms? Are you famous, infamous, a normal citizen or a mystery?":
+      "Infamous. I bear the name of an infamous pirate captain. Most people that meet me instantly recognize the name and fear me. ",
+    "What is your home Realm like? What is something you love about it? What about it do you wish would change?":
+      "My realm consists of scattered farms and villages with bustling cities. There is mostly peace among the people. While growing up as a simple farmer, I found myself bored with the peace and monotony. And after years of strife and turbulence, those are the qualities I’ve learned to admire most about my realm. ",
+  },
+  {
+    Timestamp: "2020/12/21 9:09:51 PM EST",
+    Username: "",
+    "Before we start, what is your real life name?": "Suzye",
+    "What is your character's name?": "Archangel Segwael",
+    "What is your race?": "Elf",
+    "What is your role?": "Cleric",
+    "What is your descriptor?": "Wealthy",
+    "What is your strength?": "A cunning, strategic mind",
+    "What is your flaw?": "A pursuing gang of mercenaries",
+    "What is your drive?": "Rise to the gods, become divine",
+    "Throughout your life, you have gained a title, nickname or honorific. What is it, and how did you gain it?":
+      "Archangel",
+    "While everyone is excited to be invited to the great Yule Festival, there is another motivation that drives you to attend. What is it?":
+      "I am setting a trap for the mercenaries that have been pursuing me. No one will survive the night.",
+    "Of all the places you’ve seen on your travels, one stands out and you long to return. Where is it and why?":
+      "",
+    "How are you known within the Realms? Are you famous, infamous, a normal citizen or a mystery?":
+      "A mystery ",
+    "What is your home Realm like? What is something you love about it? What about it do you wish would change?":
+      "It's basically Heaven. I live in a castle made of air there, surrounded by fruitful gardens and expansive vineyards.",
+  },
+  {
+    Timestamp: "2020/12/26 3:12:27 PM EST",
+    Username: "alizamiller.25@gmail.com",
+    "Before we start, what is your real life name?": "Aliza",
+    "What is your character's name?": "",
+    "What is your race?": "Elf",
+    "What is your role?": "Rogue",
+    "What is your descriptor?": "Charismatic",
+    "What is your strength?": "Astonishingly fast reflexes",
+    "What is your flaw?": "Impulsive, short-sighted desires",
+    "What is your drive?": "Become a champion, unbeatable",
+    "Throughout your life, you have gained a title, nickname or honorific. What is it, and how did you gain it?":
+      "",
+    "While everyone is excited to be invited to the great Yule Festival, there is another motivation that drives you to attend. What is it?":
+      "",
+    "Of all the places you’ve seen on your travels, one stands out and you long to return. Where is it and why?":
+      "",
+    "How are you known within the Realms? Are you famous, infamous, a normal citizen or a mystery?":
+      "Infamous mystery- blamed for thefts but  never had anything to pin me on ",
+    "What is your home Realm like? What is something you love about it? What about it do you wish would change?":
+      "Wood elf - so my home realm is a hidden community in the treetops deep in the forbidden fae  forest ",
+  },
+];
+
+export default data;
